23e7cf3274deff14fac940d7e5187a15c8c399c6
John-Anthony Elenis <johnanthonyelenis@gmail.com>
2019-07-14 15:38:38 -0400

    finished cp.c

--- cp.c
+++ cp.c
@@ -4,18 +4,47 @@
 #include <string.h>
 #include <fcntl.h> 
 #include <stdlib.h>
+#include <errno.h> 
+
+
 
 int main(int argc, char* argv[]) {
 		
 	int src_fd;
 	int dest_fd;
+	
+	if (argc < 3) {
+	  printf("Please enter as src followed by a dest\n");
+	  return 0;
+	}
 
 	src_fd = open(argv[1], O_RDONLY);
+	if (src_fd == -1) {
+		printf("%s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
 	dest_fd = open(argv[2], O_CREAT | O_WRONLY);
-//	numWritten = write(fd, buf, numRead); // write the string excluding '\0'
+	if (dest_fd == -1) {
+		printf("%s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
 
+	ssize_t numWritten, numRead;
+	int len = 1000; // 1 MB
+	char * buf = malloc(len+1);
 
+	while((numRead = read(src_fd, buf, len)) > 0 ){
+		buf[numRead] = '\0';
+		printf("%s\n",buf);
+		numWritten = write(dest_fd, buf, numRead); // write the string excluding '\0'
+	}
+	if (numRead == -1) {
+		printf("%s\n", strerror(errno));
+	}	
+
+	if (buf != NULL) free(buf);
 	close(src_fd);
-	clos(dest_fd);	
+	close(dest_fd);	
 	return 0;
 }
