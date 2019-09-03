169ff7d3445faf4231f4067eec1e946c14af9505
John-Anthony Elenis <johnanthonyelenis@gmail.com>
2019-09-03 12:22:46 -0400

    added comments to multithread BST

--- Chapter_30/multithread_bst.c
+++ Chapter_30/multithread_bst.c
@@ -1,8 +1,14 @@
+/**
+ * Simple multi-threaded BST implementation. Uses functions specified by lpi
+ */
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * BST node, with mutex locks for multi-threading
+ */
 typedef struct Node {
 	pthread_mutex_t mtx;
 	char key;
@@ -15,13 +21,20 @@ typedef struct Tree {
 	Node* root;
 	pthread_mutex_t mtx;
 } Tree;
-Tree tre;
 
+Tree tre; // Tree stucture containing pointer to root of BST
+
+/**
+ * Creates a Tree structure
+ */
 void initialize(Tree* tre) {
 	tre->root = NULL;
 	pthread_mutex_init(&(tre->mtx), NULL);	
 }
 
+/**
+ * Function to easily allocate and create BST node
+ */
 Node* create_node(char key, void* val) {
 	Node *leaf;
 	leaf = malloc(sizeof(Node));
@@ -34,7 +47,9 @@ Node* create_node(char key, void* val) {
 	return leaf;
 }
 
-
+/**
+ * Returns the value of a node in the pointer pointed to by value.
+ */
 int lookup (Tree* t, char key, void **value) {
 	int done = 0;	
 	int found = 0;	
@@ -76,20 +91,16 @@ int lookup (Tree* t, char key, void **value) {
 
 }
 
-void printTree(Node* root) {
-	if (root == NULL) {
-		printf("(null)\n");
-		return;
-	}
-	printf("here %c\n", root->key);
-	printTree(root->left);
-	printTree(root->right);
-}
 
+/**
+ * inserts a node into the BST into the BST pointed to by t
+ */
 void add(Tree* t, char key, void* val) {
 	int done = 0; // use to skip loop and unlock mutexs
 
 	if (t->root == NULL) {
+		/* lock the tree so that other root nodes are not 
+		added at the same time */
 		t->root = create_node(key, val);
 		return;
 	}
@@ -123,6 +134,11 @@ void add(Tree* t, char key, void* val) {
 	}
 }
 
+/**
+ * Inserts the subtree pointed to by leaf at the next poistion in the tree
+ * pointed to by root
+ *
+ */
 void insert_right(Node* root, Node* leaf) {
 	if (leaf == NULL) {
 		return;	
@@ -144,32 +160,44 @@ void insert_right(Node* root, Node* leaf) {
 
 
 }
+
+/**
+ * Deletes a node in tree in tree if the nodes key matches key
+ */
 void delete(Tree* tree, char key) {
 	Node* cur = tree->root;
 	Node* next;
 	Node* prev = NULL;	
-	int done = 0;
+	int done = 0; // use flag to exit loop and clean up locks
 
-
+	/* iterate throught the BST keeping a reference to the previous node.
+	 Once when a node is visited it is locked. The node is unlocked when 
+	 the reference to the previous node is updated */
 	while (cur && !done) {
+		// lock this node until prev no longer holds a reference 
 		if(pthread_mutex_lock(&cur->mtx) != 0) {
 			perror("lock");
 			return;
 		}	
 
 		if (key == cur->key) {
-			if (prev == NULL) { // root
+			if (prev == NULL) { 
+				// lock the tree remove root 
 				if(pthread_mutex_lock(&tree->mtx) != 0) {
 					perror("lock");
 					return;
 				}		
+				/* place the right sub-tree in the next
+				 available position after the left subtree */
 				insert_right(cur->left, cur->right);
-				tree->root = cur->left;
+
+				// make left subtree the root
+				tree->root = cur->left; 				
 				if(pthread_mutex_unlock(&tree->mtx) != 0) {
 					perror("unlock");
 					return;
 				}		
-				done = 1;
+				done = 1; // clean up other locks
 			} else if (prev->left == cur) { // left subchild
 				if (cur->left) {
 					prev->left = cur->left;
@@ -183,17 +211,18 @@ void delete(Tree* tree, char key) {
 				} else 
 					prev->right = cur->right;
 			}
-			
-			
-			printf("found %c\n", cur->key);
 			done = 1;
-		} else if (key <= cur->key && cur->left != NULL) {
+		} else if (key <= cur->key && cur->left != NULL) { 
+			// key is on left
 			next = cur->left;
 		} else if (cur->right != NULL) {
+		       	// key is on right
 			next = cur->right;
 		} else {
+			// could not locate key
 			done = 1;
 		}
+		// unlock the previous node if it exists
 		if(prev && pthread_mutex_unlock(&prev->mtx) != 0) {
 			perror("unlock");
 			return;
@@ -203,6 +232,7 @@ void delete(Tree* tree, char key) {
 
 		
 	}
+	// current mutex is still locked, unlock it
 	if(pthread_mutex_unlock(&cur->mtx) != 0) {
 		perror("unlock");
 		return;
@@ -210,6 +240,11 @@ void delete(Tree* tree, char key) {
 
 
 }
+
+/**
+ * Function run seperate threads, part of driver program to test multi-thread
+ * BST
+ */
 static void* threadFunc(void* arg) {
 	char c  = *(char*)arg;
 	void* ptr = malloc(1);;
@@ -218,6 +253,22 @@ static void* threadFunc(void* arg) {
 	printf("%c done\n", c);
 }
 
+/**
+ * Prints a BST in preorder pointed to by root
+ */
+void printTree(Node* root) {
+	if (root == NULL) {
+		printf("(null)\n");
+		return;
+	}
+	printf("here %c\n", root->key);
+	printTree(root->left);
+	printTree(root->right);
+}
+
+/**
+ * Driver program to test multi-thread BST
+ */
 int main () {
 	pthread_t thread_A, thread_B;
 	initialize(&tre);
@@ -227,6 +278,7 @@ int main () {
 	char z = 'z';
 	add(&tre, 'e', NULL);
 
+	// run two threads to asynchronously add to BST
 	pthread_create(&thread_A, NULL, threadFunc, &b);
 	pthread_create(&thread_B, NULL, threadFunc, &z);
 
@@ -236,7 +288,7 @@ int main () {
 
 	// print the tree before 
 	printTree(tre.root);	
-	delete(&tre, 'e');
+	delete(&tre, 'e'); // delete a node
 	printf("**** After Delete ****\n");
 	// print the tree after deleting
 	printTree(tre.root);	
