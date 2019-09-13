171f80e47ff003373b79c74138be888091be00f7
John-Anthony Elenis <johnanthonyelenis@gmail.com>
2019-09-13 14:51:24 -0400

    created seqnum client

---
+++ Chapter_46/seqnum_client.c
@@ -0,0 +1,54 @@
+#include "server.h"
+#include <string.h>
+
+static void removeQueue() {
+
+}
+
+/**
+ * Requests a sequence of numbers from the server
+ * using the length specified in argv[2]
+ */
+int main (int argc, char* argv[]) {
+	Message req, resp;
+	int msqid, pid;
+	int seq_len = 0;
+
+	if (argc < 2) {
+		printf("Enter the length of the sequence\n");
+		exit(EXIT_FAILURE);
+	}
+
+	seq_len = strtod(argv[1], NULL);
+	if (seq_len <= 0) {
+		printf("Length must be greater than zero\n");
+		exit(EXIT_FAILURE);
+	}
+
+	msqid = msgget(KEY, S_IWUSR | S_IRUSR);
+	if (msqid == -1) {
+		perror("msgget");
+		exit(EXIT_FAILURE);
+	}
+
+	// create request
+	pid = getpid();
+	req.mtype = 1; //server addr	
+	req.from = pid;
+	req.seq = seq_len;
+	
+	// send a message to the server
+	if (msgsnd(msqid, &req, MSG_SIZE, 0) == -1) {
+		perror("msgsnd");
+		exit(EXIT_FAILURE);
+	}
+
+	// block and wait for a response addressed to this pid
+	if (msgrcv(msqid, &resp, MSG_SIZE, pid, 0) == -1) {
+		perror("msgrcv");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Received sequence: %d\n", resp.seq);
+
+}
