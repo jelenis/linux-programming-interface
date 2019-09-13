ed1291cb315cb114abe9dc359ed160950f22cc95
John-Anthony Elenis <johnanthonyelenis@gmail.com>
2019-09-13 19:05:51 -0400

    removed sleep call that was leftover for debugging

--- Chapter_46/seqnum_client.c
+++ Chapter_46/seqnum_client.c
@@ -45,7 +45,7 @@ int main (int argc, char* argv[]) {
 		perror("msgsnd");
 		exit(EXIT_FAILURE);
 	}
-	sleep(20);
+	
 	// block and wait for a response that has the address of this pid
 	if (msgrcv(msqid, &resp, MSG_SIZE, pid, 0) == -1) {
 		perror("msgrcv");
